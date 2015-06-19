463ca427e16cd507e80cb93df80a168e295d8cb8
Dimitar Kaludov <mitakyt@gmail.com>
2015-06-20 00:09:28 +0300

    Make logo less lame

---
+++ ClubChallengeBeta/Models/UserViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClubChallengeBeta.Models
+{
+    public class SimpleUserViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool IsOwner { get; set; }
+    }
+}
