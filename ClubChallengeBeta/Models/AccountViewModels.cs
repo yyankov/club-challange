9c9001564cfcce2df5783eab555b0fb1f92aaae5
Dimitar Kaludov <mitakyt@gmail.com>
2015-06-21 19:56:20 +0300

    Update user profile

--- ClubChallengeBeta/Models/AccountViewModels.cs
+++ ClubChallengeBeta/Models/AccountViewModels.cs
@@ -72,4 +72,37 @@ namespace ClubChallengeBeta.Models
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
     }
+
+
+    public class UpdateAccountViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Please enter valid phone number")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public UpdateAccountViewModel()
+        {
+            UserName = "";
+            EmailAddress = "";
+            PhoneNumber = "";
+        }
+
+        public UpdateAccountViewModel(AspNetUser dbUser)
+        {
+            UserName = dbUser.UserName;
+            EmailAddress = dbUser.Email;
+            PhoneNumber = dbUser.PhoneNumber;
+        }
+    }
 }
