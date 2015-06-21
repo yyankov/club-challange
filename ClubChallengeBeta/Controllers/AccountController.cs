951f5b71761cf83d29837cf54558675507cf8d57
Dimitar Kaludov <mitakyt@gmail.com>
2015-06-21 16:42:09 +0300

    Add icons to more navbar-dropdown options

--- ClubChallengeBeta/Controllers/AccountController.cs
+++ ClubChallengeBeta/Controllers/AccountController.cs
@@ -69,32 +69,6 @@ namespace ClubChallengeBeta.Controllers
             return View();
         }
 
-        //
-        //// POST: /Account/Register
-        //[HttpPost]
-        //[AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Register(RegisterViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var user = new ApplicationUser() { UserName = model.UserName };
-        //        var result = await UserManager.CreateAsync(user, model.Password);
-        //        if (result.Succeeded)
-        //        {
-        //            await SignInAsync(user, isPersistent: false);
-        //            return RedirectToAction("Index", "Home");
-        //        }
-        //        else
-        //        {
-        //            AddErrors(result);
-        //        }
-        //    }
-
-        //    // If we got this far, something failed, redisplay form
-        //    return View(model);
-        //}
-
 
         [HttpPost]
         [AllowAnonymous]
@@ -325,7 +299,7 @@ namespace ClubChallengeBeta.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login");
         }
 
         //
