9f2f2b796f2e8e935e648f55968aed5ef20ca6c4
Dimitar Kaludov <mitakyt@gmail.com>
2015-07-04 20:44:10 +0300

    Use minified files

--- ClubChallengeBeta/App_Start/BundleConfig.cs
+++ ClubChallengeBeta/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace ClubChallengeBeta
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.min.js"));
+                        "~/Scripts/jquery-2.1.4.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
