c62c770ec9319f5506383b3b843c01d690864adb
Dimitar Kaludov <mitakyt@gmail.com>
2015-07-07 22:38:28 +0300

    auth settings

--- ClubChallengeBeta/App_Start/Startup.Auth.cs
+++ ClubChallengeBeta/App_Start/Startup.Auth.cs
@@ -10,50 +10,41 @@ namespace ClubChallengeBeta
     public partial class Startup
     {
         #region static
-        static string microsoftKey = ConfigurationManager.AppSettings.Get("microsoftKey");
         static string microsoftId = ConfigurationManager.AppSettings.Get("microsoftId");
-        static string googleKey = ConfigurationManager.AppSettings.Get("googleKey");
-        static string googleId = ConfigurationManager.AppSettings.Get("googleId");
-        static string facebookKey = ConfigurationManager.AppSettings.Get("facebookKey");
+        static string microsoftKey = ConfigurationManager.AppSettings.Get("microsoftKey");
         static string facebookId = ConfigurationManager.AppSettings.Get("facebookId");
-        static string twitterKey = ConfigurationManager.AppSettings.Get("twitterKey");
-        static string twitterId = ConfigurationManager.AppSettings.Get("twitterId");
+        static string facebookKey = ConfigurationManager.AppSettings.Get("facebookKey");
+        static string googleId = ConfigurationManager.AppSettings.Get("googleId");
+        static string googleKey = ConfigurationManager.AppSettings.Get("googleKey");
         #endregion
 
-        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Enable the application to use a cookie to store information for the signed in user
+
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login")
             });
-            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-            // Uncomment the following lines to enable logging in with third party login providers
+
             app.UseMicrosoftAccountAuthentication(
-                clientId: "0000000044151A29",
-                clientSecret: "3yiAaWL7detOCYyXQYlcPqgXl9wldC80");
+                clientId: microsoftId,
+                clientSecret: microsoftKey);
 
 
-            app.UseTwitterAuthentication(
-               consumerKey: "QdQTTvZqu4cqyqfOQ4iqGeeLV ",
-               consumerSecret: "snSWZaT3CImCUfYMn9yhStERMQi3uMyPTvk5kUmLuYkV5Jcq4j");
-
-
-            app.UseFacebookAuthentication(
-                appId: "372763689600824",
-                appSecret: "f858477b14730957b76b552a36102a76");
+            //app.UseFacebookAuthentication(
+            //    appId: facebookId,
+            //    appSecret: facebookKey);
 
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             {
-                ClientId = "1051623227327-raujnh43rfcv5t76bnh7anee951rq4jc.apps.googleusercontent.com",
-                ClientSecret = "q8PMpoLwHC8I_W9IZhN0DN1u",
+                ClientId = googleId,
+                ClientSecret = googleKey,
                 Provider = new GoogleOAuth2AuthenticationProvider()
             });
-
-            //app.UseGoogleAuthentication();
         }
     }
 }
